c398cf5b09e7c2ae6cf0cff082b6124cdffe756d
Victoria <bernard.victoria22@gmail.com>
2019-05-22 14:11:58 -0500

    Fixes to demo working

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * Example someHost is set up to respond with JSON and HTML according to the path
  *  */
-const someHost = 'http://victoriacf.tk' //"http://workers-tooling.cf/fake-origin"
-const someJSONURL = someHost + '/json'
-const someHTMLURL = someHost + '/html'
+const someHost = 'http://victoriacf.tk/demos' //equivalent to what runs on workers-tooling.cf/demos
+const someJSONURL = someHost + '/requests/json'
+const someHTMLURL = someHost + '/static/html'
 const someJSONToSend = {
   results: ['default data to send'],
   errors: null,
